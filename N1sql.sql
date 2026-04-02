b632c99b6ebbb492ac9d83cab2ae306e77183f04
BaoDepTrai25 <nguyenduongthebao25@gmail.com>
2026-04-02 15:49:59 +0700

    Add files via upload

---
+++ N1sql.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE login_php CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+USE login_php;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
